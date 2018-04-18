bc9042048bea72bf33ee19eaa0a4735d5d0cc29e
Yaman Shrestha <yamanshrestha@Yamans-MacBook-Pro.local>
2018-04-18 19:31:42 -0400

    added componentDidMount

--- restaurantrecs/src/App.js
+++ restaurantrecs/src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import RestaurantList from './RestaurantList.js'
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import RestaurantList from "./RestaurantList.js";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    let googleData = []
-    //make call to google maps API here and store as props for react object
-    let axios = require("axios");
-
-    function getMapsData() {
-      axios
+  componentDidMount() {
+    axios
       .get(
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.0293,-78.4767&radius=500&type=restaurant&key=AIzaSyDia5OKW5vOzkUjZoBq0QJGjn-FUypLPW0"
       )
       .then(response => {
         googleData = response.data.results;
+        //googleData.map(datum=>{console.log(datum.name + " has a rating of " + datum.rating+" and price level of "+datum.price_level+" is open now: "+datum.opening_hours.open_now);});
       })
       .catch(error => {
         console.log(error);
-    });
-    }
+      });
+  }
 
-    getMapsData();
+  render() {
+    let googleData = [];
+    //make call to google maps API here and store as props for react object
+    let axios = require("axios");
 
     return (
       <div className="App">
@@ -33,7 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <RestaurantList googleData={googleData}/>
+        <RestaurantList googleData={googleData} />
       </div>
     );
   }
--- restaurantrecs/src/RestaurantList.js
+++ restaurantrecs/src/RestaurantList.js
@@ -4,20 +4,25 @@ import React, { Component } from "react";
   data: object of restaurants and scores
 */
 
+/* States
+  
+
+*/
+
 export default class RestaurantList extends Component {
+  // constructor
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
   // render method
   render() {
     // return an array of react components by mapping 
     // through array and turning them into react components
-    let bullets = this.props.googleData.map(googleData => {
-      return (
-        <div>
-        <ul>
-          <li>{" "}{googleData.name}: {googleData.rating}{" "}</li>
-        </ul>
-        </div>
-      );
-    });
+    let bullets =["Hello", "How are you doing"];
+    //this.props.googleData.map(googleData => { });
 
     return (
       <div>
