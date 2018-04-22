7893bdf43673f5aa41a80be8b64e5bc42f25b3c0
Yaman Shrestha <yamanshrestha@yamans-mbp.lan>
2018-04-22 16:22:44 -0400

    something wrong with mapping

--- restaurantrecs/src/App.js
+++ restaurantrecs/src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import RestaurantList from "./RestaurantList.js";
 import "./App.css";
+let axios = require("axios");
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
   componentDidMount() {
     axios
       .get(
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.0293,-78.4767&radius=500&type=restaurant&key=AIzaSyDia5OKW5vOzkUjZoBq0QJGjn-FUypLPW0"
       )
       .then(response => {
-        googleData = response.data.results;
+        this.setState({
+          data: response.data.results
+        });
         //googleData.map(datum=>{console.log(datum.name + " has a rating of " + datum.rating+" and price level of "+datum.price_level+" is open now: "+datum.opening_hours.open_now);});
       })
       .catch(error => {
@@ -19,9 +29,28 @@ class App extends Component {
   }
 
   render() {
-    let googleData = [];
-    //make call to google maps API here and store as props for react object
-    let axios = require("axios");
+    //   const Test = ({ resturants }) => (
+    //     <div>
+    //       {resturants.map(rest => (
+    //         <div className="rest" key={rest.name}>
+    //           {rest.name}
+    //         </div>
+    //       ))}
+    //     </div>
+    //   );
+
+    //   return (
+    //     <div>
+    //       <ul>
+    //         <Test resturant={restruants} />
+    //       </ul>
+    //     </div>
+    //   );
+    // }
+
+    let { resturantArray } = this.state.data.map(resturant => {
+      return <RestaurantList resturant={resturant} />;
+    });
 
     return (
       <div className="App">
@@ -32,7 +61,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <RestaurantList googleData={googleData} />
+        {resturantArray}
       </div>
     );
   }
--- restaurantrecs/src/RestaurantList.js
+++ restaurantrecs/src/RestaurantList.js
@@ -11,23 +11,27 @@ import React, { Component } from "react";
 
 export default class RestaurantList extends Component {
   // constructor
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
 
   // render method
   render() {
-    // return an array of react components by mapping 
-    // through array and turning them into react components
-    let bullets =["Hello", "How are you doing"];
-    //this.props.googleData.map(googleData => { });
-
+    let { resturantData } = this.props; //curly bracket becasue its an object
+    console.log({ resturantData });
     return (
       <div>
-          {bullets}
+        <h6> Name: </h6>
+        <p> {resturantData.name} </p>
+
+        <h6> Rating: </h6>
+        <p> {resturantData.rating} </p>
+
+        <h6> Price Level: </h6>
+        <p> {resturantData.price_level} </p>
+
+        <h6> Is Open?: </h6>
+        <p> {resturantData.opening_hours.open_now} </p>
       </div>
     );
   }
 }
+
+//googleData.map(datum=>{console.log(datum.name + " has a rating of " + datum.rating+" and price level of "+datum.price_level+" is open now: "+datum.opening_hours.open_now);});
